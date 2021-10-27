1d7fbb6e4096913dc52c5bc9e7f4d4b9e2d6b388
Yusuke Wada <yusuke@kamawada.com>
2021-10-27 17:34:53 +0900

    Use query param

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export function middleware(req: NextRequest) {
-  const url = req.url
+  const message = req.nextUrl.searchParams.get("message") || " NOTHING "
   const res = NextResponse.next()
-  res.headers.append("X-Meesage-By-Middleware", "Hello! from " + url)
+  res.headers.append("X-Meesage-From-Middleware", "Message: " + message)
   return res
 }
